f668f5b12d621e84a0f99ceb6297901a53207ef9
takomazin <uta_manato@koushi-intec.co.jp>
2025-04-09 16:22:59 +0900

    15課題

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,17 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//インスタンス化
+		Car_Chapter15 car = new Car_Chapter15();
+		
+		car.gearChange(3);
+		
+		car.run();
+
+	}
+
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,27 @@
+package kadai_015;
+
+//車クラス
+public class Car_Chapter15 {
+	
+	// フィールド（内部データ）
+	private int gear = 1;  // 1速から5速のギアを表す
+	
+	private int speed = 10;  //ギアチェンジ後の速度を表す
+	
+	//ギアの値により速度を変える
+	public void gearChange( int afterGear ) {
+		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
+		gear = afterGear;
+	}
+	
+	
+	//ギアチェンジ後の速度を表示する
+	public void run() {
+		if(gear <= 5 || gear >= 1 ) {
+			System.out.println("速度は時速" + gear*speed + "Kmです");
+		}else {
+			System.out.println("速度は時速"+ speed + "Kmです");
+		}
+	}
+
+}
