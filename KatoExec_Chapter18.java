59ec81fdfc671b679caf829ac924253a2c57041b
takomazin <uta_manato@koushi-intec.co.jp>
2025-04-15 14:16:42 +0900

    Add files via upload

---
+++ KatoExec_Chapter18.java
@@ -0,0 +1,25 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		
+		taro.setGivenName();
+		taro.execIntroduce();
+		
+		ichiro.setGivenName();
+		ichiro.execIntroduce();
+		
+		hanako.setGivenName();
+		hanako.execIntroduce();
+
+	}
+
+}
